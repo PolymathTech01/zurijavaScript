5a25d36694288d9ccae33e3bdddd45c34b1eaff9
PolymathTech01 <polymathalli@gmail.com>
2022-06-21 11:12:58 +0100

    first javascript code zuri

---
+++ index.js
@@ -0,0 +1,13 @@
+const name = 'Alli Olarinde';
+const height = 5.5;
+const country = 'Nigeria';
+
+console.log(
+  'My name is ' +
+    name +
+    ' and I am ' +
+    height +
+    ' meters tall. I am from ' +
+    country +
+    '.'
+);
